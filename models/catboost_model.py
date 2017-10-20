94007d6d6dde03e44dac8aa37b8ff6d607a0a10a
mmalates <malatesta.mike@gmail.com>
2017-10-20 01:33:46 -0700

    updated catboost with new features

--- models/catboost_model.py
+++ models/catboost_model.py
@@ -15,13 +15,6 @@ def dateparse(x): return pd.datetime.strptime(x, '%Y-%m-%d')
 df = pd.read_csv('~/golf-staff/data/train_clean.csv',
                  parse_dates=['date'], date_parser=dateparse)
 
-df = df.sort_values('date')
-
-features = ['DOW', 'DOY', 'temp_high', 'hum_avg',
-            'prec', 'wind_avg', 'vis_low', 'sea_avg', 'dew_avg']
-# features = ['DOW', 'DOY', 'temp_high',
-# 'temp_low', 'hum_avg', 'wind_avg', 'prec'] # best features so far
-
 
 def datetime_series(df, col):
     '''
@@ -57,47 +50,69 @@ seasonal_smooth = smooth(rounds_decomposition.seasonal, 30)
 rounds_residual_smooth = rounds_decomposition.observed - seasonal_smooth
 
 
+# feature engineering
+df = df.sort_values('date')
+df['year'] = df.date.dt.year
+df['month'] = df.date.dt.month
+
+# create rain boolean
+df['rain'] = df.prec.values > 0
+
+# create cumulative rain feature
+prec_arr = df.prec.values
+prec_arr = ([0.0] + list(prec_arr))[:-1]
+df['cum_prec'] = np.array(prec_arr) + df.prec.values
+
+# create rain the day before boolean
+rain_arr = df.rain.values
+rain_dbf = ([False] + list(rain_arr))[:-1]
+df['rain_db4'] = np.array(rain_dbf)
+
+# remove outliers
 df = df[df.rounds < 200]
 
+# remove mondays because they are all holidays
+not_mondays = df.DOW != 0
+df = df[not_mondays]
+
+# create feature list for modeling
+features = ['year', 'month', 'rain', 'DOW', 'DOY', 'temp_high', 'temp_low', 'hum_avg',
+            'wind_high', 'prec', 'sea_high', 'dew_avg', 'vis_low', 'rain_db4', 'cum_prec']
+
+# select features from training data
 X = df[features]
-for feat1 in features:
-    for feat2 in features:
-        if feat1 != feat2:
-            X[feat1 + '_' + feat2] = X[feat1] * X[feat2]
 
-for feat1 in features:
-    for feat2 in features:
-        if feat1 != feat2:
-            X[feat1 + '^2_' + feat2] = X[feat1]**2 * X[feat2]
+# use to investigate feature interactions
+# for feat1 in features:
+#     for feat2 in features:
+#         if feat1 != feat2:
+#             X[feat1 + '_' + feat2] = X[feat1] * X[feat2]
+#
+# for feat1 in features:
+#     for feat2 in features:
+#         if feat1 != feat2:
+#             X[feat1 + '^2_' + feat2] = X[feat1]**2 * X[feat2]
 
+# more features
 X['temp_high_sqrt'] = np.sqrt(X['temp_high'])
-# X['temp_low_sqrt'] = np.sqrt(X['temp_low'])
 X['hum_avg_sqrt'] = np.sqrt(X['hum_avg'])
-# X['prec_DOY'] = X['prec'] * X['DOY']
-# X['hum_avg^2_prec'] = X['hum_avg']**2 * X['prec']
-# X['hum_avg_prec'] = X['hum_avg'] * X['prec']
-# X['temp_high^2_hum_avg'] = X['temp_high']**2 * X['hum_avg']
-# X['temp_high_DOW'] = X['temp_high'] * X['DOW']
-# features_new = ['DOY', 'temp_high^2_DOY', 'DOY^2_temp_high', 'temp_high_sqrt', 'temp_high', 'DOY^2_hum_avg', 'DOY_hum_avg', 'DOY_temp_high', 'hum_avg^2_DOY', 'temp_high^2_hum_avg',
-# 'temp_high_hum_avg', 'hum_avg', 'DOY^2_DOW', 'hum_avg_sqrt', 'DOY_DOW', 'DOW^2_DOY', 'hum_avg^2_DOW', 'temp_high^2_DOW', 'hum_avg_prec', 'prec^2_hum_avg', 'prec_DOW', 'DOW_temp_high']
+X['prec_sqrt'] = np.sqrt(X['prec'])
 
-# features_new2 = ['DOW^2_temp_high', 'prec^2_DOW', 'prec_hum_avg',
-#  'prec', 'hum_avg^2_prec', 'temp_high^2_prec', 'hum_avg_DOW', 'hum_avg^2_temp_high']
 
-# new = features_new + features_new2
+# target variable
 y = df['rounds'].values
 # y = pd.Series(rounds_residual_smooth)
 grid = []
 
 
 def CAT_model():
-    cat = CatBoostRegressor()
+    cat = CatBoostRegressor(iterations=1000, learning_rate=0.02)
 # approx_on_full_history=False, gradient_iterations=1,
     # loss_function='RMSE', iterations=1000, learning_rate=0.03, thread_count=None, depth=8, border_count=255)
     # cat = CatBoostRegressor(iterations=estimators,
     # depth=depth,
     # learning_rate=learn_rate, loss_function='RMSE')
-    kf = KFold(n_splits=8, shuffle=False)
+    kf = KFold(n_splits=10, shuffle=False)
     error = []
     print "starting stuff"
     for train_index, test_index in kf.split(y):
@@ -117,7 +132,7 @@ def CAT_model():
 
 CAT_model()
 
-with open('/home/mike/golf-staff/models/CAT_results.txt', 'w') as f:
+with open('grid_results/CAT_results.txt', 'w') as f:
     for row in sorted(grid):
         f.write(str(row))
         f.write('\n\n')
